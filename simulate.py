be406b65d473e58d5ab9be77502daabd33dfae23
pau-antonio <pauantoniosoler@gmail.com>
2020-12-07 18:41:19 +0100

    minor modifications

--- simulate.py
+++ simulate.py
@@ -1,55 +1,49 @@
 # Exemple per executar: 
-#   python simulate.py Stonks Stonks Stonks2 Stonks2 100
+#   python simulate.py Stonks Stonks Stonks2 Stonks2
+# Sortida:
+#   Stonks won with seeds: 43913 70491 41637 65191
+# 
+#   Stonks2 won with seeds: 39428 20517 29384 42524 24253 54463
+# 
+#   Stonks won 4 times 40.00%
+#   Stonks2 won 6 times 60.00%
 
 import subprocess
 import random
 import os
 import sys
-from multiprocessing import Pool, Lock, current_process, Value
-from collections import defaultdict
 
-N_THREADS = 6 # Recomanació: 2*(nombre de nuclis) - 1 o 2*(nombre de nuclis) - 2
+N_PARTIDES = 10
 
-total = Value('i', 0)
-N_PARTIDES = None
+def play(p1, p2, p3, p4):
+    players = set()
+    players.add(p1)
+    players.add(p2)
+    players.add(p3)
+    players.add(p4)
+    wins = {}
+    total = 0
+    seeds = {}
+    for p in players:
+        wins[p] = 0
+        seeds[p] = set()
+    for times in range(N_PARTIDES):
+        print(f"{100.0*times/N_PARTIDES:2.2f}%")
+        seed = random.randint(1,99999)
+        command = f"./Game -s {seed} {p1} {p2} {p3} {p4} --input default.cnf --output out.res"
+        res = subprocess.run(list(command.split(" ")), capture_output=True)
+        for line in res.stderr.decode().split("\n"):
+            if "got top score" in line:
+                for p in players:
+                    if p in line.split():
+                        wins[p] += 1
+                        total += 1
+                        seeds[p].add(seed)
+    print("100%\n\n")
+    for p in sorted(players):
+        print(f"{p} won with seeds: {' '.join([str(x) for x in seeds[p]])}\n")
+    print('\n'.join([f"{p} won {wins[p]} times {100.0*wins[p]/total:2.2f}%" for p in sorted(players)]))
 
-def player(x):
-  global total
-  pi1,pi2,pi3,pi4,seed = x
-  command = f"./Game -s {seed} {pi1} {pi2} {pi3} {pi4} --input default.cnf --output /dev/null"
-  res = subprocess.run(list(command.split(" ")), capture_output=True)
-  for line in res.stderr.decode().splitlines():
-      if "got top score" in line:
-          for p in set([pi1,pi2,pi3,pi4]):
-              if p in line.split():
-                total.value += 1
-                if "1" in current_process().name:
-                  pct = f"{100.0*total.value/N_PARTIDES:2.2f}"
-                  print(f"{total.value:5}/{N_PARTIDES} ( {pct.rjust(5)}% )")
-                return (p, seed)
-
-def main():
-  global N_PARTIDES
-  assert(len(sys.argv) >= 6)
-  
-  p1,p2,p3,p4, N_PARTIDES = sys.argv[1:]
-  
-  N_PARTIDES = int(N_PARTIDES)
-
-  with Pool(processes=N_THREADS) as pool:
-    result = pool.map(player, ((p1,p2,p3,p4,i) for i in random.choices(range(99999), k=N_PARTIDES)))
-    res = defaultdict(list) 
-    for i, j in result: 
-        res[i].append(j)
-    print("\n\n")
-    for p in res:
-      print(f"Player {p} won with seeds {res[p]}\n")
-      
-    print("\n\nAND:")
-    
-    for p in res:
-      print(f"Player {p:12} got {len(res[p])} wins, ie {100.0*len(res[p])/N_PARTIDES:2.2f}%")
-    
-      
 if __name__ == '__main__':
-  main()
+    assert(len(sys.argv) >= 5)
+    play(*sys.argv[1:])
